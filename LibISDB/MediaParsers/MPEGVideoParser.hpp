0eb8b945e33fc28a79f321a869e2e1d4f861590d
DBCTRADO <hdustest@gmail.com>
2018-02-22 22:24:39 +0900

    Copyright 表記の年を2018に更新

--- LibISDB/MediaParsers/MPEGVideoParser.hpp
+++ LibISDB/MediaParsers/MPEGVideoParser.hpp
@@ -1,6 +1,6 @@
 /*
   LibISDB
-  Copyright(c) 2017 DBCTRADO
+  Copyright(c) 2017-2018 DBCTRADO
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
