4a6c0808b36b61a08e48629813de7218b27ae088
DBCTRADO <hdustest@gmail.com>
2019-01-30 22:12:03 +0900

    Copyright 表記の年を2019に更新

--- LibISDB/Windows/Viewer/DirectShow/VideoRenderers/VideoRenderer.hpp
+++ LibISDB/Windows/Viewer/DirectShow/VideoRenderers/VideoRenderer.hpp
@@ -1,6 +1,6 @@
 /*
   LibISDB
-  Copyright(c) 2017-2018 DBCTRADO
+  Copyright(c) 2017-2019 DBCTRADO
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
