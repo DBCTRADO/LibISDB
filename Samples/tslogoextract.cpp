f6aff72edae23b6d148ef3502fb242f63717e492
DBCTRADO <hdustest@gmail.com>
2023-01-24 23:18:27 +0900

    tslogoextract: パレットが間違っているのを修正

--- Samples/tslogoextract.cpp
+++ Samples/tslogoextract.cpp
@@ -145,7 +145,7 @@ bool LogoExtractEngine::WritePNGWithPLTE(LibISDB::Stream &Stream, const std::uin
 		{255,   0,   0, 255},
 		{  0, 255,   0, 255},
 		{255, 255,   0, 255},
-		{  0,   0,   0, 255},
+		{  0,   0, 255, 255},
 		{255,   0, 255, 255},
 		{  0, 255, 255, 255},
 		{255, 255, 255, 255},
